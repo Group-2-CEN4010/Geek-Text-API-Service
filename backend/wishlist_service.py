b5d3b9e93286310ea026a3df7d7da1a6857eb6ea
The Kapitan. <159403179+chrisb9085@users.noreply.github.com>
2026-01-25 22:22:55 -0500

    Renamed routes to fit RESTful naming conventions

--- backend/wishlist_service.py
+++ backend/wishlist_service.py
@@ -2,19 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/wishlist/add', methods=['POST'])
+@app.route('/wishlist', methods=['POST'])
 def add_to_wishlist():
     # TODO: Create a function to add items to the wishlist => POST
 
     return "Empty messsage."
 
-@app.route('wishlist/remove', methods=['DELETE'])
-def remove_from_wishlist():
+@app.route('/wishlist/<int:item_id>', methods=['DELETE'])
+def remove_from_wishlist(item_id):
     # TODO: Create a function to remove items from the wishlist => DELETE
 
     return "Empty messsage."
 
-@app.route('wishlist/get', methods=['GET'])
+@app.route('/wishlist', methods=['GET'])
 def retrieve_wishlist():
     # TODO: Create a function to retrieve wishlist items => GET
 
