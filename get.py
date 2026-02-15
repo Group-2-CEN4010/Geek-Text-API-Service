0685c02a7ae169692899464f2d5967560c0fda0a
cyntia-salt <cyntiadocs555@gmail.com>
2026-02-15 18:02:53 -0500

    Create get.py

---
+++ get.py
@@ -0,0 +1,7 @@
+from flask import Flask 
+group2 = Flask(__name__) #start website
+@group2.route("/get") #creates URL
+def my_route(): #define what happens when someone visits the URL
+return {"message": "This is my GET route for Sprint 2"}
+if __name__ == "__main__":
+  get.run(debug=True)
