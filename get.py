ca76eb04661ec83b42278d26f5b1ff2c61f616a6
cyntia-salt <cyntiadocs555@gmail.com>
2026-02-15 20:33:17 -0500

    Update get.py

--- get.py
+++ get.py
@@ -2,7 +2,7 @@ from flask import Flask
 group2 = Flask(__name__) #start website
 @group2.route("/get") #creates URL
 def my_route(): #define what happens when someone visits the URL
-    return {"message": "This is my GET route for Sprint 2"}
+    return {"message": "This is my get route for Sprint 2"}
   
 if __name__ == "__main__":
   group2.run(debug=True)
