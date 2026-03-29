313b514e9ce1543a8c55cba2ac86902896461287
The Kapitan. <159403179+chrisb9085@users.noreply.github.com>
2026-03-29 16:32:28 -0400

    Updated main.py to account for new services

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,8 @@ from app import app
 # Import service modules to register their routes on the shared app instance
 import wishlist_service
 import book_details_service
+import login_service
+import book_browsing
 
 if __name__ == "__main__":
     app.run(debug=True)
